cb8c86abd43c0419552c2cf2bf4099ea9d4a05c7
Konovalov Maxim <43151027+KaymeKaydex@users.noreply.github.com>
2024-05-13 16:53:48 +0300

    Fix providers lint warnings & make 0.0.11 release (#25)
    Co-authored-by: maxim-konovalov <maksim.konovalov@vk.team>

--- providers.go
+++ providers.go
@@ -28,16 +28,16 @@ func (e *EmptyLogger) Warn(_ context.Context, _ string)  {}
 
 type StdoutLogger struct{}
 
-func (e *StdoutLogger) Info(ctx context.Context, msg string) {
+func (e *StdoutLogger) Info(_ context.Context, msg string) {
 	log.Println(msg)
 }
-func (e *StdoutLogger) Debug(ctx context.Context, msg string) {
+func (e *StdoutLogger) Debug(_ context.Context, msg string) {
 	log.Println(msg)
 }
-func (e *StdoutLogger) Error(ctx context.Context, msg string) {
+func (e *StdoutLogger) Error(_ context.Context, msg string) {
 	log.Println(msg)
 }
-func (e *StdoutLogger) Warn(ctx context.Context, msg string) {
+func (e *StdoutLogger) Warn(_ context.Context, msg string) {
 	log.Println(msg)
 }
 
@@ -54,9 +54,9 @@ type MetricsProvider interface {
 // you can embed this type and realize just some metrics
 type EmptyMetrics struct{}
 
-func (e *EmptyMetrics) CronDiscoveryEvent(ok bool, duration time.Duration, reason string) {}
-func (e *EmptyMetrics) RetryOnCall(reason string)                                         {}
-func (e *EmptyMetrics) RequestDuration(duration time.Duration, ok bool, mapReduce bool)   {}
+func (e *EmptyMetrics) CronDiscoveryEvent(_ bool, _ time.Duration, _ string) {}
+func (e *EmptyMetrics) RetryOnCall(_ string)                                 {}
+func (e *EmptyMetrics) RequestDuration(_ time.Duration, _ bool, _ bool)      {}
 
 // TopologyProvider is external module that can lookup current topology of cluster
 // it might be etcd/config/consul or smth else
